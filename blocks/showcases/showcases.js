3f595d798001ddaeb3fcaee23a89f93affb8626b
Alexander Saar <alexander.saar@gmail.com>
2022-10-19 22:48:36 +0200

    fix showcase styling

--- blocks/showcases/showcases.js
+++ blocks/showcases/showcases.js
@@ -14,12 +14,12 @@ export default async function decorate(block) {
 
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.querySelector('.cards').children].forEach((row) => {
+  [...block.querySelector('.showcase').children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'showcases-card-image';
+      else div.className = 'showcases-card-body';
     });
     ul.append(li);
   });
