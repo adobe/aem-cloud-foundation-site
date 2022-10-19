102f4c99297304f9cde839219f288a1de10a6015
Alexander Saar <alexander.saar@gmail.com>
2022-10-19 20:56:09 +0200

    columns image transition

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,20 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('enter');
+      }
+    });
+  });
+
+  block.querySelectorAll(':scope > div > div').forEach((cell) => {
+    if (!cell.previousElementSibling) cell.classList.add('columns-left');
+    if (!cell.nextElementSibling) cell.classList.add('columns-right');
+  });
+  block.querySelectorAll('img').forEach((img) => {
+    observer.observe(img);
+  });
 }
