e01bfff72a3a027e118535aa9d14f27a3a7aef3d
Peter A <52969899+rootpea@users.noreply.github.com>
2023-04-06 00:29:30 +0200

    update boilerplate for header js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,4 @@
-import { getMetaData, decorateIcons } from '../../scripts/lib-franklin.js';
+import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
